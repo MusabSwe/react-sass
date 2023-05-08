38ef6c8edcca9d65d828f63bdfd55628058ac1c5
Musab <malqarninet@gmail.com>
2023-05-08 11:04:19 +0300

    react with sass 1. Install sass package 2. Write variables for the fonts & colors 3. Add style for the entire app 4. Implment the Header

---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import classes from "./Header.module.scss";
+import { BiMenuAltRight } from "react-icons/bi";
+import { AiOutlineClose } from "react-icons/ai";
+const Header = () => {
+    return (
+        <header className={classes.header}>
+            <div className={classes.header_content}>
+                <h2>navbar</h2>
+            </div>
+            <nav className={classes.header__content__nav}>
+                <ul>
+                    <li>
+                        <a href="/">PageOne</a>
+                    </li>
+                    <li>
+                        <a href="/">PageTwo</a>
+                    </li>
+                    <li>
+                        <a href="/">PageThree</a>
+                    </li>
+                </ul>
+                <button>CTA Page</button>
+            </nav>
+            <div className={classes.header__content__toggle}>
+                <BiMenuAltRight />
+            </div>
+        </header>
+    );
+}
+
+export default Header;
