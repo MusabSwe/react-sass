7eac9088cc215e6f701be12e795531d29a9a7eab
Musab <malqarninet@gmail.com>
2023-05-08 13:45:32 +0300

    Implement header and add toggle state for the menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,81 @@
-import classes from "./Header.module.scss";
+import React, { useEffect, useState } from "react";
+
 import { BiMenuAltRight } from "react-icons/bi";
 import { AiOutlineClose } from "react-icons/ai";
+
+import classes from "./Header.module.scss";
+import { Link, useNavigate } from "react-router-dom";
+
 const Header = () => {
+    const navigate = useNavigate();
+    const [menuOpen, setMenuOpen] = useState(false);
+    const [size, setSize] = useState({
+        width: undefined,
+        height: undefined,
+    });
+
+    useEffect(() => {
+        const handleResize = () => {
+            setSize({
+                width: window.innerWidth,
+                height: window.innerHeight,
+            });
+        };
+        window.addEventListener("resize", handleResize);
+
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    useEffect(() => {
+        if (size.width > 768 && menuOpen) {
+            setMenuOpen(false);
+        }
+    }, [size.width, menuOpen]);
+
+    const menuToggleHandler = () => {
+        setMenuOpen((p) => !p);
+    };
+
+    const ctaClickHandler = () => {
+        menuToggleHandler();
+        navigate("/page-cta");
+    };
+
     return (
         <header className={classes.header}>
             <div className={classes.header__content}>
-                <h2 className={classes.header__content__logo}>navbar</h2>
-                <nav className={classes.header__content__nav}>
+                <Link to="/" className={classes.header__content__logo}>
+                    navbar
+                </Link>
+                <nav
+                    className={`${classes.header__content__nav} ${menuOpen ? classes.isMenu : ""
+                        }`}
+                >
                     <ul>
                         <li>
-                            <a href="/">PageOne</a>
+                            <Link to="/page-one" >
+                                PageOne
+                            </Link>
                         </li>
                         <li>
-                            <a href="/">PageTwo</a>
+                            <Link to="/page-two" >
+                                PageTwo
+                            </Link>
                         </li>
                         <li>
-                            <a href="/">PageThree</a>
+                            <Link to="/page-three" >
+                                PageThree
+                            </Link>
                         </li>
                     </ul>
                     <button>CTA Page</button>
                 </nav>
                 <div className={classes.header__content__toggle}>
-                    <BiMenuAltRight />
+                    {menuOpen ? <BiMenuAltRight onClick={menuToggleHandler} /> : <AiOutlineClose onClick={menuToggleHandler} />}
                 </div>
             </div>
         </header>
     );
-}
+};
 
 export default Header;
