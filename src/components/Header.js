0fad35ebd4b47e0514049892f24c95d2e0bd2afc
Musab <malqarninet@gmail.com>
2023-05-08 12:33:39 +0300

    Header style

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,25 +4,25 @@ import { AiOutlineClose } from "react-icons/ai";
 const Header = () => {
     return (
         <header className={classes.header}>
-            <div className={classes.header_content}>
-                <h2>navbar</h2>
-            </div>
-            <nav className={classes.header__content__nav}>
-                <ul>
-                    <li>
-                        <a href="/">PageOne</a>
-                    </li>
-                    <li>
-                        <a href="/">PageTwo</a>
-                    </li>
-                    <li>
-                        <a href="/">PageThree</a>
-                    </li>
-                </ul>
-                <button>CTA Page</button>
-            </nav>
-            <div className={classes.header__content__toggle}>
-                <BiMenuAltRight />
+            <div className={classes.header__content}>
+                <h2 className={classes.header__content__logo}>navbar</h2>
+                <nav className={classes.header__content__nav}>
+                    <ul>
+                        <li>
+                            <a href="/">PageOne</a>
+                        </li>
+                        <li>
+                            <a href="/">PageTwo</a>
+                        </li>
+                        <li>
+                            <a href="/">PageThree</a>
+                        </li>
+                    </ul>
+                    <button>CTA Page</button>
+                </nav>
+                <div className={classes.header__content__toggle}>
+                    <BiMenuAltRight />
+                </div>
             </div>
         </header>
     );
